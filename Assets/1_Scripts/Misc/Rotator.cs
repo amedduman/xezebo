73ab5c2843f647fbdc301d05cbf74e2466d6660a
amed <amedduman@protonmail.com>
2022-06-27 01:57:31 +0300

    Improve main menu

---
+++ Assets/1_Scripts/Misc/Rotator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] float _timeToRotate = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DORotate(new Vector3(0,30,0), _timeToRotate).
+        SetLoops(-1, LoopType.Incremental).
+        SetEase(Ease.Linear);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
